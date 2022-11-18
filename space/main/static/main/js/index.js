1ee45c1b68ce135cb047d622581a7c8ca9c2ada1
Damir <rbk_74@mail.ru>
2022-11-18 19:33:42 +0300

    images line-up for recent posts on index.html

--- space/main/static/main/js/index.js
+++ space/main/static/main/js/index.js
@@ -1,20 +1,28 @@
-(function() {
-	// Lining up images in recent posts by the upper line
-	var x = window.matchMedia("(max-width: 768px)");
-	x.addListener(function() {
+(function () {
+	"use strict"
+	
+	window.addEventListener("load", function () {
 		const recentPostsTextContents = select(".recent-post-card-content > a > div", true);
-		//Reset height of divs on mobile devices
-		if(x.matches) {
-			recentPostsTextContents[0].style.height = null;
-			recentPostsTextContents[1].style.height = null;
-		} else {
-			// Figuring out the highest div in recent post
-			const firstDivHeight = getComputedStyle(recentPostsTextContents[0]).height;
-			const secondDivHeight = getComputedStyle(recentPostsTextContents[1]).height;
-			const highestDivHeight = parseInt(firstDivHeight) > parseInt(secondDivHeight) ? firstDivHeight : secondDivHeight;
-			// Settings the height to each div
-			recentPostsTextContents[0].style.height = highestDivHeight;
-			recentPostsTextContents[1].style.height = highestDivHeight;
+	
+		// Lining up images in recent posts by the upper line
+		function lineUpImages(x) {
+			//Reset height of divs on mobile devices
+			if(x.matches) {
+				recentPostsTextContents[0].style.height = null;
+				recentPostsTextContents[1].style.height = null;
+			} else {
+				// Figuring out the highest div in recent post
+				const firstDivHeight = getComputedStyle(recentPostsTextContents[0]).height;
+				const secondDivHeight = getComputedStyle(recentPostsTextContents[1]).height;
+				const highestDivHeight = parseInt(firstDivHeight) > parseInt(secondDivHeight) ? firstDivHeight : secondDivHeight;
+				// Settings the height to each div
+				recentPostsTextContents[0].style.height = highestDivHeight;
+				recentPostsTextContents[1].style.height = highestDivHeight;
+			}		
 		}
+		
+		var x = window.matchMedia("(max-width: 768px)");
+		lineUpImages(x);
+		x.addListener(lineUpImages);
 	});
 })()
